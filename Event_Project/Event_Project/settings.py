acd6550f053a6fa2ef993478d859aa36f6156b21
adityapratapsingh728-eng <adityapratapsingh728@gmail.com>
2026-01-28 11:26:40 +0530

    Update settings.py

--- Event_Project/Event_Project/settings.py
+++ Event_Project/Event_Project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'import_export',
+    'anymail', 
 ]
 
 MIDDLEWARE = [
@@ -167,4 +168,5 @@ orig_getaddrinfo = socket.getaddrinfo
 def getaddrinfo_ipv4(host, port, family=0, type=0, proto=0, flags=0):
     return orig_getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
 socket.getaddrinfo = getaddrinfo_ipv4
+
 
